d2e4474dd4cd7222042e72a09040401791971b1d
bxcar <malanchukdima@mail.ru>
2018-05-21 14:31:56 +0300

    done

--- wp-content/themes/sprout-spoon/404.php
+++ wp-content/themes/sprout-spoon/404.php
@@ -13,7 +13,6 @@
 						<h1><?php esc_html_e( '404', 'sprout-spoon' ); ?></h1>
 						<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'sprout-spoon' ); ?></p>
 						<?php get_search_form(); ?>
-						
 					</div>
 					
 				</div>
